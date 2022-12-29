a07398eb59ed44d70f3a298e185d35e8e7184678
pratee <dev2@gmail.com>
2022-12-29 17:49:26 +0530

    I C 1

---
+++ DEMO2/src/COM/RB/UTILITY/UTILITY1.java
@@ -0,0 +1,13 @@
+package COM.RB.UTILITY;
+import java.util.Date;
+public class UTILITY1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println("hi");
+	
+
+	}
+
+}
